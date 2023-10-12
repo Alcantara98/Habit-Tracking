5fd178b893f6432c5908713c6ff7bb877020af95
ZorDeluxe <zoren.delacruz27@gmail.com>
2023-10-12 23:07:18 +1300

    Upgraded nodejs to version 18

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,10 @@
-import { createApp } from 'react';
-import { render } from 'react-dom';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-render(<App />, document.getElementById('app'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
